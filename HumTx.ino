9a8f8c4a31d9d39f238c0f023100806a3b67da07
Juan Taba Gasha <jatg81@gmail.com>
2018-12-15 12:22:41 -0500

    e

--- HumTx.ino
+++ HumTx.ino
@@ -3,9 +3,8 @@
 #include <PinChangeInt.h>
 //#include <RF69Mod.h>
 
-#define ECHO_PIN      3
-#define TRIG_PIN      4
-
+#define TRIG_PIN      3
+#define ECHO_PIN      4
 #define DHTPin        5  
 #define COOLER        6
 #define PUMP          7
@@ -13,19 +12,43 @@
 #define SW_NOT_TANK   9 
 #define SW_HI_LEVEL   14
 
-#define SET_HI_LEVEL  20
-#define SET_LOW_LEVEL 40
 #define RF_freq           RF69_433MHZ                                                               //Frecuencia de transmision 433Mhz
+#define LO_LEVEL_MM   50    //max 78mm
+#define HI_LEVEL_MM   24   // sw 35 mm
+//300ml 52mm
+//bombeo 75 segundos
 
 
-HCSR04 mySensor;
+HCSR04 sensorLevel;
 DHT dht;
-int i;
+float tankLevel;
+short levelmm;
+int i,errCode,status;
 const int nodeIdEmonPi=5;                                                                            // Id del EmonPi
 const int nodeId = 13;                                                                               // emonTx RFM12B nodo ID
 const int netGroup = 210;                                                                            // emonTx RFM12B  grupo ID
 unsigned short distance;
 float humAmb,tempAmb;
+
+void printdata(){
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print(" Tanque: ");
+  Serial.print(tankLevel);
+  Serial.print ("  Temperatura: ");
+  Serial.print(tempAmb);
+  Serial.print("   Humedad: ");
+  Serial.print(humAmb);
+  Serial.print("  SW: ");
+  Serial.println(!digitalRead(SW_HI_LEVEL));
+
+}
+
+short getTankLevel (){
+  short distance;
+  distance= sensorLevel.readAccurateDisctanceInMm(); //aprox 400ms
+  return (100*(distance - LO_LEVEL_MM) ) / (HI_LEVEL_MM - LO_LEVEL_MM); 
+}
 
 void setup() {    
 
@@ -34,12 +57,13 @@ void setup() {
   pinMode(SW_HI_LEVEL, INPUT);
   digitalWrite(SW_HI_LEVEL, HIGH);
   pinMode(COOLER, OUTPUT);
-  digitalWrite(COOLER, HIGH);
+  digitalWrite(COOLER, LOW);
   pinMode(PUMP, OUTPUT);
   digitalWrite(PUMP, HIGH);
   pinMode(LED, OUTPUT);
-  mySensor.init(TRIG_PIN, ECHO_PIN);
-  //sensorLevel.setDelayBetweenAvgMeasurementsInMs(100);
+  digitalWrite(LED, HIGH);
+  sensorLevel.init(TRIG_PIN, ECHO_PIN);
+  sensorLevel.setDelayBetweenAvgMeasurementsInMs(35);
   dht.setup(DHTPin);
 
   for(int j=0;j<15;j++){
@@ -51,19 +75,54 @@ void setup() {
 }
 
 void loop(){
-  i++;
-  distance = mySensor.readDisctanceInMm(); //aprox 400ms
-  tempAmb=dht.getTemperature();
-  tempAmb=isnan(tempAmb)?-127 :tempAmb;
-  humAmb=dht.getHumidity();
-  humAmb=isnan(humAmb)?-127 :humAmb;
-  Serial.print(i);
-  Serial.print(") Distance: ");
-  Serial.print(distance);
-  Serial.print ("  Temperatura: ");
-  Serial.print(tempAmb);
-  Serial.print("   Humedad: ");
-  Serial.println(humAmb);
+
+  /*if (!digitalRead(SW_NOT_TANK) && digitalRead(SW_HI_LEVEL)){
+    digitalWrite(COOLER, LOW);
+    status=1;
+  } else digitalWrite(COOLER, HIGH);
+  */
+  if (!digitalRead(PUMP)) digitalWrite(COOLER,HIGH);
+  //levelmm=sensorLevel.readAccurateDisctanceInMm();
+  levelmm=sensorLevel.readAvgDisctanceInMm(40);
+  Serial.print("Distance: ");
+  Serial.println(levelmm);
+  if(levelmm<40 || !digitalRead(SW_HI_LEVEL)){ //40mm
+    digitalWrite(PUMP, LOW);
+    digitalWrite(COOLER, HIGH);
+  }
+  if(levelmm>85){
+    digitalWrite(PUMP, HIGH);
+  }
+  delay(500);
+
+  
+/*
+  if(distance > LO_LEVEL_MM){ //70
+    tankLevel = 0;
+    if(distance > LO_LEVEL_MM + 10){
+      status=2;
+      errCode=20;
+    }
+  }
+  else {
+    if(distance < HI_LEVEL_MM){//42
+      tankLevel = 100;
+      if(distance < HI_LEVEL_MM - 10 || !digitalRead(SW_HI_LEVEL)){
+        status=2;
+        errCode=30;
+      }
+    }
+    else tankLevel=(100*(float(distance) - LO_LEVEL_MM) ) / (HI_LEVEL_MM - LO_LEVEL_MM);
+  }
+
+  
+  if (tankLevel==100){
+    digitalWrite(PUMP, LOW);
+    do{
+      distance = sensorLevel.readAccurateDisctanceInMm();
+      tankLevel=(100*(float(distance) - LO_LEVEL_MM) ) / (HI_LEVEL_MM - LO_LEVEL_MM);
+    } while()
+  }
   delay(1000);
   // Do something with measured distance value
-}
+*/}
